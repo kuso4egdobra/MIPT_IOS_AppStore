e8dea27146a9173a0d66607944d056561e8766e4
Korotkov Andrey <akorotkov1234@yandex.ru>
2021-10-18 13:32:55 +0300

    Initial Commit

---
+++ MIPT_IOS_AppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MIPT_IOS_AppStore
+//
+//  Created by Андрей Коротков on 18/10/2021.
+//  Copyright © 2021 Андрей Коротков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
